84fb6b265c3acbecaf9a064dadfc02bbc2f6c877
bhuvan-360ithub <bhuvankumar@360ithub.com>
2025-07-11 11:55:54 +0530

    initial commit

---
+++ lib/app/modules/courses/views/courses_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:lms_mobileapp/app/routes/app_pages.dart';
+
+import '../controllers/courses_controller.dart';
+
+class CoursesView extends GetView<CoursesController> {
+  const CoursesView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CoursesView'),
+        centerTitle: true,
+      ),
+      body:  Center(
+        // Home button
+        child: ElevatedButton(onPressed: () => Get.toNamed(Routes.HOME), child: Text('Go to Home')),
+      ),
+    );
+  }
+}
